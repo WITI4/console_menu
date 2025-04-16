b6009cb289149f417c76bea5ef569333b079e499
denis <ar.denis.by@gmail.com>
2025-04-17 02:05:35 +0300

    + secondaryMenu + new cursorPosition realization

--- cursor_menu.cpp
+++ cursor_menu.cpp
@@ -3,14 +3,16 @@
 #include <conio.h>
 #include <iostream>
 
+#include "cursor_menu.h"
+
 static int lastSelectedPosition = 0;
 
-void setCursorPos(int y) {
-    static HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
-    SetConsoleCursorPosition(h, { 0, (short)y });
+void setCursorPos(int y, int x) {
+    COORD pos = { short(x), short(y) };
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
 }
 
-int showMenu(const std::string& title, const std::string opts[], int count, bool rememberPosition = false) {
+int main_showMenu(const std::string& title, const std::string opts[], int count, bool rememberPosition) {
     srand(time(NULL));
     HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
 
@@ -21,7 +23,7 @@ int showMenu(const std::string& title, const std::string opts[], int count, bool
     if (ch >= count) ch = 0;
 
     for (int i = 0; i < count; i++) {
-        setCursorPos(i + 2);
+        setCursorPos(i + 2, 0);
         std::cout << "  " << opts[i];
     }
 
@@ -29,13 +31,13 @@ int showMenu(const std::string& title, const std::string opts[], int count, bool
     while (true) {
         if (prev != ch) {
             if (prev >= 0) {
-                setCursorPos(prev + 2);
+                setCursorPos(prev + 2, 0);
                 SetConsoleTextAttribute(h, 7);
                 std::cout << "  " << opts[prev];
             }
 
-            setCursorPos(ch + 2);
-            int pipeColor = rand() % 6 + 9;
+            setCursorPos(ch + 2, 0);
+            int pipeColor = 9;
             int backgroundColor = (pipeColor - 8) << 4;
 
             SetConsoleTextAttribute(h, pipeColor);
@@ -50,15 +52,65 @@ int showMenu(const std::string& title, const std::string opts[], int count, bool
         switch (_getch()) {
         case 224: case 0:
             switch (_getch()) {
-            case 72: ch = (ch - 1 + count) % count; break; // ¬верх
-            case 80: ch = (ch + 1) % count; break; // ¬низ
+            case 72: ch = (ch - 1 + count) % count; break;
+            case 80: ch = (ch + 1) % count; break;
             } break;
         case 13:
             if (rememberPosition) {
                 lastSelectedPosition = ch;
             }
-            setCursorPos(count + 3);
-            return ch; // Enter
+            setCursorPos(count + 3, 0);
+            return ch;
+        }
+    }
+}
+
+int secondary_showMenu(const std::string opts[], int count, int startY) {
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    GetConsoleScreenBufferInfo(hConsole, &csbi);
+    WORD originalColors = csbi.wAttributes;
+
+    int savedCursorY = csbi.dwCursorPosition.Y;
+
+    for (int i = 0; i < count; i++) {
+        setCursorPos(startY + i, MENU_INDENT);
+        std::cout << "  " << opts[i];
+    }
+
+    int ch = 0;
+    int prev = -1;
+
+    while (true) {
+        if (prev != ch) {
+            if (prev >= 0) {
+                setCursorPos(startY + prev, MENU_INDENT);
+                SetConsoleTextAttribute(hConsole, originalColors);
+                std::cout << "  " << opts[prev];
+            }
+
+            int pipeColor = 9;
+            int backgroundColor = (pipeColor - 8) << 4;
+
+            setCursorPos(startY + ch, MENU_INDENT);
+            SetConsoleTextAttribute(hConsole, pipeColor);
+            std::cout << " |";
+            SetConsoleTextAttribute(hConsole, backgroundColor | 7);
+            std::cout << opts[ch];
+            SetConsoleTextAttribute(hConsole, 7);
+
+            prev = ch;
+        }
+
+        switch (_getch()) {
+        case 224: case 0:
+            switch (_getch()) {
+            case 72: ch = (ch - 1 + count) % count; break;
+            case 80: ch = (ch + 1) % count; break;
+            } break;
+        case 13:
+            setCursorPos(count + 3, MENU_INDENT);
+            return ch;
         }
     }
 }
--- cursor_menu.h
+++ cursor_menu.h
@@ -1,5 +1,8 @@
 #pragma once
 #include <string>
 
-void setCursorPos(int y);
-int showMenu(const std::string& title, const std::string opts[], int count, bool rememberPosition = false);
+const int MENU_INDENT = 2;
+
+void setCursorPos(int y, int x = MENU_INDENT);
+int main_showMenu(const std::string& title, const std::string opts[], int count, bool rememberPosition = false);
+int secondary_showMenu(const std::string opts[], int count, int startY = 6);
